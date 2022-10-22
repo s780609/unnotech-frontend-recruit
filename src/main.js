6a28ed21a886d3b8475c3e255566a301199cd267
HsuTingHuan <s780609@gmail.com>
2022-10-22 18:43:07 +0800

    style: Clean up unnecessary code

--- src/main.js
+++ src/main.js
@@ -11,9 +11,6 @@ import AddPage from "./pages/AddPage.vue";
 import BookDetailPage from "./pages/BookDetailPage.vue";
 import BookEditPage from "./pages/BookEditPage.vue";
 
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
 const routes = [
   { path: "/", name: "BookListPage", component: BookListPage },
   { path: "/add", name: "AddPage", component: AddPage },
@@ -21,21 +18,14 @@ const routes = [
   { path: "/edit/:id", name: "BookEditPage", component: BookEditPage },
 ];
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHashHistory(),
-  routes, // short for `routes: routes`
+  routes, 
 });
 
 const app = createApp(App);
 
 app.use(BootstrapVue3);
-
-// Make sure to _use_ the router instance to make the
-// whole app router-aware.
 app.use(router);
 
 app.mount("#app");
